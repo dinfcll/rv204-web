613b5d341d78cfb76d23929708f2af20befff703
Olivier Lafleur <olivier.lafleur@gmail.com>
2014-09-29 13:54:22 -0400

    Crop image

--- info-helper.php
+++ info-helper.php
@@ -21,6 +21,8 @@ function retourneImage()
     $uploadfile = $uploaddir . basename($_FILES['image']['name']);
 
     if (move_uploaded_file($_FILES['image']['tmp_name'], $uploadfile)) {
+        $uploadfile = cropImage($uploadfile, $uploaddir, $_FILES['image']['type']);
+
         return file_get_contents($uploadfile);
     } else {
         return "";
@@ -57,14 +59,14 @@ function maj($utilisateur, $monacces)
 
 function insererUtilisateur($retourFormulaire, $monacces)
 {
-    if($retourFormulaire['prenom'] != "" and $retourFormulaire['nomfamille'] != "") {
+    if ($retourFormulaire['prenom'] != "" and $retourFormulaire['nomfamille'] != "") {
         $prenom = $retourFormulaire['prenom'];
         $nomfamille = $retourFormulaire['nomfamille'];
     } else {
         return messageErreur("Vous devez entrer un nom complet");
     }
 
-    if($retourFormulaire['username'] != "") {
+    if ($retourFormulaire['username'] != "") {
         $username = $retourFormulaire['username'];
     } else {
         return messageErreur("Vous devez entrer un nom d'utilisateur");
@@ -76,13 +78,13 @@ function insererUtilisateur($retourFormulaire, $monacces)
         return messageErreur("Les mots de passe sont différents");
     }
 
-    if($retourFormulaire['password1'] == "") {
+    if ($retourFormulaire['password1'] == "") {
         return messageErreur("Vous devez entrer un mot de passe");
     }
 
     $couleur = $retourFormulaire['couleur'];
 
-    if(isset($retourFormulaire['admin'])) {
+    if (isset($retourFormulaire['admin'])) {
         $isAdmin = 1;
     } else {
         $isAdmin = 0;
@@ -107,4 +109,31 @@ function messageSucces($message)
 function messageInfo($message)
 {
     return "<div class=\"alert alert-info\">" . $message . "</div>";
+}
+
+function cropImage($uploadfile, $uploaddir, $type)
+{
+    $targ_w = 300;
+    $targ_h = 400;
+    $jpeg_quality = 100;
+    $output_filename = $uploaddir . "cropped.jpg";
+
+    if (strpos($type, 'jpeg') !== false) {
+        $img_r = imagecreatefromjpeg($uploadfile);
+    } else {
+        if (strpos($type, 'gif') !== false) {
+            $img_r = imagecreatefromjpeg($uploadfile);
+        } else {
+            $img_r = imagecreatefrompng($uploadfile);
+        }
+    }
+
+    $dst_r = ImageCreateTrueColor($targ_w, $targ_h);
+
+    imagecopyresampled($dst_r, $img_r, 0, 0, $_POST['x'], $_POST['y'],
+        $targ_w, $targ_h, $_POST['w'], $_POST['h']);
+
+    imagejpeg($dst_r, $output_filename, $jpeg_quality);
+
+    return $output_filename;
 }
